1171f8021b716d99e8c548f35c14409e14fc7389
Artem <milkartem2@mail.ru>
2024-02-22 13:07:37 +0300

    hw9

---
+++ lesson9-2/reader-serivce/src/main/java/com/example/readerserivce/ReaderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.readerserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReaderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderServiceApplication.class, args);
+	}
+
+}
---
+++ lesson9-2/reader-serivce/src/test/java/com/example/readerserivce/ReaderSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readerserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
